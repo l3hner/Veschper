e5adf56d6b8e6275114c4998595574e4f544a877
l3hner <jonaslehner7@gmail.com>
2024-11-27 10:38:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ yesNoSelect.addEventListener('change', () => {
 // Rücksetzen-Button mit Passwortabfrage
 resetButton.addEventListener('click', () => {
     const password = prompt('Bitte gib das Passwort ein, um die Daten zurückzusetzen:');
-    if (password === 'geheimesPasswort') { // Hier kannst du das Passwort nach Belieben ändern
+    if (password === 'Meister') { // Hier kannst du das Passwort nach Belieben ändern
         // Tagesübersicht speichern
         const date = new Date().toLocaleDateString();
         dailyOverview.innerHTML += `<p>${date}: ${total} Leberkäswecken</p>`;
