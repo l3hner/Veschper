c7b2d7e36f35b0987786249e5b52d9715c831215
l3hner <jonaslehner7@gmail.com>
2024-11-28 09:10:39 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-// Firebase SDKs einbinden
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore-compat.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics-compat.js";
+// Importiere Firebase und Firestore
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
-// Firebase-Konfiguration einfügen (ersetze die Platzhalter mit deinen echten Firebase-Daten)
+// Firebase-Konfiguration
 const firebaseConfig = {
     apiKey: "AIzaSyCUznRu9r4_TF4bUoFHA7ABc9s6HchcFCQ",
     authDomain: "veschper-10337.firebaseapp.com",
@@ -17,7 +16,6 @@ const firebaseConfig = {
 // Firebase initialisieren
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
-// Die `db` Variable global verfügbar machen
+// Die `db`-Variable global verfügbar machen
 window.db = db;
